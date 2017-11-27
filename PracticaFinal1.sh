24b48ae88e755b006df03bef73d57a75beb1f4b2
Carlos <carlos-rafael.perez@alumnos.unican.es>
2017-11-27 11:44:02 +0100

    Final commit

--- PracticaFinal1.sh
+++ PracticaFinal1.sh
@@ -5,15 +5,12 @@
  do 
 	for mes in Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre
 	do 
+		cod="_"
 		if [ $year == '2015' ] && [ $mes == 'Mayo' ]; then break; fi # Rompe el bucle a partir de Mayo de 2015.
-		if [ $year == '2009' ] && [ $mes != 'Diciembre' ]; then # Para la meses de Enero-Noviembre de 2009 la URL es distinta
-			curl -o temp/$mes\_$year.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/$year/$mes\%20$year.zip &>> aux.log	
-		else
-			curl -o temp/$mes\_$year.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/$year/$mes\_$year.zip &>> aux.log
-		fi
-		unzip -q temp/$mes\_$year.zip -d temp/$mes\_$year &>> aux.log # Descomprime los ficheros en un directorio llamado *Mes_Año*
+		if [ $year == '2009' ] && [ $mes != 'Diciembre' ]; then cod="%20"; fi # Para la meses de Enero-Noviembre de 2009 la URL es distinta
+		curl -o temp/$mes\_$year.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/$year/$mes$cod$year.zip &>> aux.log
+		unzip -q -j temp/$mes\_$year.zip -d temp/$mes\_$year &>> aux.log # Descomprime los ficheros en un directorio llamado *Mes_Año*
 		rm temp/$mes\_$year.zip # Borra el archivo comprimido
-		mv temp/$mes\_$year/*/T_127P*.txt temp/$mes\_$year # Mueve el archivo que necesitamos a el directorio creado anteriormente *Mes_Año*.
 	done
 done
 
